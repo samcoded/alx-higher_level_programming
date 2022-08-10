192bb58122ac4ff5da98fa54e4a3e79eb46ae121
Samuel Anozie <samuelanozie10@yahoo.com>
2022-08-10 22:56:47 +0100

    task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,19 +1,8 @@
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
-
+-- List all shows in db 'hbtn_0d_tvshows'
+-- Each record show display tv_shows.title, tv_show_genres.genre_id
+-- If show doesn't have genre, display NULL
+-- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv_shows
-	LEFT JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-	LEFT JOIN tv_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
-
-SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv_shows
-	LEFT JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-	LEFT JOIN tv_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
-
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
